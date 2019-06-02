ff22494203fb8fe571ca0bad1df3942e186de3a7
skywalkeryin <skywalkeryin007@gmail.com>
2019-06-02 23:35:05 +0800

    add the static folder

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,8 +1,15 @@
 
 from flask import Blueprint
 
+
+# Set up static file folder, need to set up static_folder(in the bluePrint, web folder here is the beginning, here using the relative path),
+# if you don't set up the static_url_path, it will use the tail
+# of static_folder as the url.
+# for example:  static_folder='test/statics' ,the url will be statics
+# Only you set up the static_folder, then the static_url_path will take affect.
+
 # BluePrint
-web = Blueprint('web', __name__)
+web = Blueprint('web', __name__, static_folder='statics', static_url_path='/web/statics')
 
 
 from app.web import book
